4c9ad97a660aae78e197608fb5e03073c41897bc
Legion-fire <Legion-fire@yandex.ru>
2025-08-28 07:29:37 +0300

    реализация с помощью фор и простой тест умножение на 2

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
-public class ArrayUtils {
+public final class ArrayUtils {
+    private ArrayUtils() {}
+
+    public static <T> T[] filter(T[] src, Filter<T> filter) {
+        if (src == null) throw new NullPointerException("src is null");
+        if (filter == null) throw new NullPointerException("filter is null");
+
+        // Копируем, чтобы вернуть новый массив того же типа
+
+        T[] dst = Arrays.copyOf(src, src.length);
+        for (int i = 0; i < dst.length; i++) {
+            dst[i] = filter.apply(dst[i]);
+        }
+        return dst;
+    }
 }
